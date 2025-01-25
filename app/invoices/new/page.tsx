566952416518255310e867965795b27c62599cb0
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-01-25 18:19:54 +0530

    Added the schema and the data table

--- app/invoices/new/page.tsx
+++ app/invoices/new/page.tsx
@@ -1,15 +1,20 @@
+import { sql } from "drizzle-orm";
+import { db } from "@/db";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 
-export default function NewInvoice() {
+export default async function NewInvoice() {
+    const result = await db.execute(sql`SELECT current_database()`);
   return (
     <main className="flex flex-col justify-center h-full gap-5 max-w-5xl mx-auto my-12">
       <div className="justify-between flex">
         <h1 className="text-3xl font-bold">Create a New Invoice</h1>
       </div>
 
+      {JSON.stringify(result)}
+      
       <form className="grid gap-4 max-w-xs">
         <div>
             <Label htmlFor="name" className="block mb-2 font-semibold text-base">Billing name</Label>
