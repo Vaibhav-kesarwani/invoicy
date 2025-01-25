7c807a0f72da8e7b2919182803492fa34d147b38
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-01-25 17:33:17 +0530

    Added the new invoice button

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import {
   Table,
   TableBody,
@@ -8,59 +9,51 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { CirclePlus } from "lucide-react";
+import Link from "next/link";
 
 export default function Dashboard() {
   return (
-    <main className="flex flex-col justify-center text-center gap-5 max-w-5xl mx-auto">
-      <h1 className="text-6xl font-bold">Dashboard</h1>
+    <main className="flex flex-col justify-center text-center gap-5 max-w-5xl mx-auto my-12">
+      <div className="justify-between flex">
+        <h1 className="text-3xl font-bold">Invoices</h1>
+        <p>
+          <Button className="inline-flex gap-2" variant="ghost" asChild>
+            <Link href="/invoices/new">
+              <CirclePlus className="h-4 w-4 " />
+              Create Invoice
+            </Link>
+          </Button>
+        </p>
+      </div>
       <Table>
         <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px] p-4">
-                Date
-            </TableHead>
-            <TableHead className="p-4">
-                Customer
-            </TableHead>
-            <TableHead className="p-4">
-                Email
-            </TableHead>
-            <TableHead className="text-center p-4">
-                Status
-            </TableHead>
-            <TableHead className="text-right p-4">
-                Value
-            </TableHead>
+            <TableHead className="w-[100px] p-4">Date</TableHead>
+            <TableHead className="p-4">Customer</TableHead>
+            <TableHead className="p-4">Email</TableHead>
+            <TableHead className="text-center p-4">Status</TableHead>
+            <TableHead className="text-right p-4">Value</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           <TableRow>
             <TableCell className="font-medium text-left p-4">
-                <span className="font-semibold">
-                    10/31/2024
-                </span>
+              <span className="font-semibold">10/31/2024</span>
             </TableCell>
             <TableCell className="text-left p-4">
-                <span className="font-semibold">
-                    Yash Kumar
-                </span>
+              <span className="font-semibold">Yash Kumar</span>
             </TableCell>
             <TableCell className="text-left p-4">
-                <span>
-                    xyz@gmail.com
-                </span>
+              <span>xyz@gmail.com</span>
             </TableCell>
             <TableCell className="text-center p-4">
-                <Badge className="rounded-full">
-                    Open
-                </Badge>
+              <Badge className="rounded-full">Open</Badge>
             </TableCell>
             <TableCell className="text-right p-4">
-                <span className="font-semibold">
-                    ₹250.00
-                </span>
-            </TableCell> 
+              <span className="font-semibold">₹250.00</span>
+            </TableCell>
           </TableRow>
         </TableBody>
       </Table>
