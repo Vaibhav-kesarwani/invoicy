63dc9353142b55e8f6569019af4573f6fe24adb1
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-01-25 23:47:52 +0530

    Added the handle on submit option

--- app/invoices/new/page.tsx
+++ app/invoices/new/page.tsx
@@ -1,17 +1,34 @@
+"use client";
+
 import { createAction } from "@/app/actions";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { SyntheticEvent, useState } from "react";
 
-export default async function NewInvoice() {
+export default function NewInvoice() {
+  const [state, setState] = useState("ready");
+  async function handleOnSubmit(event: SyntheticEvent) {
+    event.preventDefault();
+    if (state === "pending") return;
+    setState("pending");
+    const target = event.target as HTMLFormElement;
+    const formData = new FormData(target);
+    await createAction(formData);
+  }
+
   return (
     <main className="flex flex-col justify-center h-full gap-5 max-w-5xl mx-auto my-12">
       <div className="justify-between flex">
         <h1 className="text-3xl font-bold">Create a New Invoice</h1>
       </div>
 
-      <form action={createAction} className="grid gap-4 max-w-xs">
+      <form
+        action={createAction}
+        onSubmit={handleOnSubmit}
+        className="grid gap-4 max-w-xs"
+      >
         <div>
           <Label htmlFor="name" className="block mb-2 font-semibold text-base">
             Billing name
