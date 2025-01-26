063b43073f377ccbc8970f4e274729b0b309a08d
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-01-26 22:31:39 +0530

    Added the container

--- app/invoices/[invoiceId]/page.tsx
+++ app/invoices/[invoiceId]/page.tsx
@@ -1,3 +1,4 @@
+import Container from "@/components/Container";
 import { Badge } from "@/components/ui/badge";
 import { db } from "@/db";
 import { Invoices } from "@/db/schema";
@@ -5,6 +6,13 @@ import { cn } from "@/lib/utils";
 import { auth } from "@clerk/nextjs/server";
 import { eq, and } from "drizzle-orm";
 import { notFound } from "next/navigation";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
 export default async function InvoicePage({
   params,
@@ -31,55 +39,67 @@ export default async function InvoicePage({
   }
 
   return (
-    <main className="h-full max-w-5xl mx-auto my-12">
-      <div className="justify-between flex mb-8">
-        <h1 className="flex items-center gap-4 text-3xl font-semibold">
-          Invoice {invoiceId}
-          <Badge
-            className={cn(
-              "rounded-full capitalize",
-              result.status === "open" && "bg-blue-500",
-              result.status === "paid" && "bg-green-600",
-              result.status === "void" && "bg-zinc-700",
-              result.status === "unpaid" && "bg-red-600"
-            )}
-          >
-            {result.status}
-          </Badge>
-        </h1>
-        <p></p>
-      </div>
+    <main className="w-full h-full">
+      <Container>
+        <div className="justify-between flex mb-8">
+          <h1 className="flex items-center gap-4 text-3xl font-semibold">
+            Invoice {invoiceId}
+            <Badge
+              className={cn(
+                "rounded-full capitalize",
+                result.status === "open" && "bg-blue-500",
+                result.status === "paid" && "bg-green-600",
+                result.status === "void" && "bg-zinc-700",
+                result.status === "unpaid" && "bg-red-600"
+              )}
+            >
+              {result.status}
+            </Badge>
+          </h1>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Button variant={"outline"}>Change status</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Billing</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
 
-      <p className="text-3xl mb-3">₹{(result.value / 100).toFixed(2)}</p>
-      <p className="text-lg mb-8">{result.description}</p>
-      <h2 className="font-bold text-lg mb-4">Billing Details</h2>
+        <p className="text-3xl mb-3">₹{(result.value / 100).toFixed(2)}</p>
+        <p className="text-lg mb-8">{result.description}</p>
+        <h2 className="font-bold text-lg mb-4">Billing Details</h2>
 
-      <ul className="grid gap-2">
-        <li className="flex gap-4">
-          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-            Invoice Id
-          </strong>
-          <span> {invoiceId} </span>
-        </li>
-        <li className="flex gap-4">
-          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-            Invoice Date
-          </strong>
-          <span>{new Date(result.createTs).toLocaleDateString()}</span>
-        </li>
-        <li className="flex gap-4">
-          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-            Billing Name
-          </strong>
-          <span></span>
-        </li>
-        <li className="flex gap-4">
-          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-            Billing Email
-          </strong>
-          <span></span>
-        </li>
-      </ul>
+        <ul className="grid gap-2">
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Invoice Id
+            </strong>
+            <span> {invoiceId} </span>
+          </li>
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Invoice Date
+            </strong>
+            <span>{new Date(result.createTs).toLocaleDateString()}</span>
+          </li>
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Billing Name
+            </strong>
+            <span></span>
+          </li>
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Billing Email
+            </strong>
+            <span></span>
+          </li>
+        </ul>
+      </Container>
     </main>
   );
 }
