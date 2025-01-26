1221b166dc97f814b842231cc69380b808b9063c
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-01-26 13:02:43 +0530

    protecting routes with clerk middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
@@ -6,6 +13,12 @@ export default function Home() {
     <main className="flex flex-col justify-center h-screen text-center gap-5 max-w-5xl mx-auto">
       <h1 className="text-6xl font-bold">Invoicy</h1>
       <p>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
         <Button asChild>
           <Link href="/dashboard">Sign In</Link>
         </Button>
