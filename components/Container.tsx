7f8a7321b4446ad2b1f751f1bd3fac8581aefc98
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-01-26 13:31:26 +0530

    Added the footer of the website

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,8 +1,6 @@
 import { cn } from "@/lib/utils";
 
-interface ContainerProps extends React.ComponentProps<"div"> {
-
-}
+type ContainerProps = React.ComponentProps<"div">
 
 const Container = ({children, className, ...props} : ContainerProps) => {
     return <div {...props} className={cn('max-w-5xl mx-auto px-5', className)}>
---
+++ components/Footer.tsx
@@ -0,0 +1,21 @@
+import Container from "./Container";
+
+const Footer = () => {
+  return (
+    <footer className="mt-6 mb-8">
+      <Container className="flex justify-between gap-4">
+        <p className="text-sm">
+          Inovicy &copy; {new Date().getFullYear()} - All Rights Reserved
+        </p>
+        <p className="text-sm">
+          Made by{" "}
+          <a href="https://vaibhavkesarwani.vercel.app/">
+            Vaibhav Kesarwani
+          </a>
+        </p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
