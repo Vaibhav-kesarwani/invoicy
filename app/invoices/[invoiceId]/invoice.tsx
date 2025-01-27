d8422730d86b608b166c7eeec1d4c547b839dbac
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-01-27 11:21:50 +0530

    Make the new invoice component use client

---
+++ app/invoices/[invoiceId]/invoice.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import Container from "@/components/Container";
+import { Badge } from "@/components/ui/badge";
+import { Invoices } from "@/db/schema";
+import { cn } from "@/lib/utils";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { AVAILABLE_STATUSES } from "@/data/invoices";
+import { updateStatus } from "@/app/actions";
+import { ChevronDown } from "lucide-react";
+
+interface InvoiceProps {
+  invoice: typeof Invoices.$inferSelect;
+}
+
+export default function Invoice({ invoice }: InvoiceProps) {
+  return (
+    <main className="w-full h-full">
+      <Container>
+        <div className="justify-between flex mb-8">
+          <h1 className="flex items-center gap-4 text-3xl font-semibold">
+            Invoice {invoice.id}
+            <Badge
+              className={cn(
+                "rounded-full capitalize",
+                invoice.status === "open" && "bg-blue-500",
+                invoice.status === "paid" && "bg-green-600",
+                invoice.status === "void" && "bg-zinc-700",
+                invoice.status === "unpaid" && "bg-red-600"
+              )}
+            >
+              {invoice.status}
+            </Badge>
+          </h1>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button className="flex items-center gap-2" variant={"outline"}>
+                Change status
+                <ChevronDown className="w-4 h-auto" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              {AVAILABLE_STATUSES.map((status) => {
+                return (
+                  <DropdownMenuItem key={status.id}>
+                    <form action={updateStatus}>
+                      <input type="hidden" value={invoice.id} name="id" />
+                      <input type="hidden" value={status.id} name="status" />
+                      <button>{status.label}</button>
+                    </form>
+                  </DropdownMenuItem>
+                );
+              })}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+
+        <p className="text-3xl mb-3">₹{(invoice.value / 100).toFixed(2)}</p>
+        <p className="text-lg mb-8">{invoice.description}</p>
+        <h2 className="font-bold text-lg mb-4">Billing Details</h2>
+
+        <ul className="grid gap-2">
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Invoice Id
+            </strong>
+            <span> {invoice.id} </span>
+          </li>
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Invoice Date
+            </strong>
+            <span>{new Date(invoice.createTs).toLocaleDateString()}</span>
+          </li>
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Billing Name
+            </strong>
+            <span></span>
+          </li>
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Billing Email
+            </strong>
+            <span></span>
+          </li>
+        </ul>
+      </Container>
+    </main>
+  );
+}
--- app/invoices/[invoiceId]/page.tsx
+++ app/invoices/[invoiceId]/page.tsx
@@ -1,21 +1,9 @@
-import Container from "@/components/Container";
-import { Badge } from "@/components/ui/badge";
 import { db } from "@/db";
 import { Invoices } from "@/db/schema";
-import { cn } from "@/lib/utils";
 import { auth } from "@clerk/nextjs/server";
 import { eq, and } from "drizzle-orm";
 import { notFound } from "next/navigation";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Button } from "@/components/ui/button";
-import { AVAILABLE_STATUSES } from "@/data/invoices";
-import { updateStatus } from "@/app/actions";
-import { ChevronDown } from "lucide-react";
+import Invoice from "./invoice";
 
 export default async function InvoicePage({
   params,
@@ -41,78 +29,5 @@ export default async function InvoicePage({
     notFound();
   }
 
-  return (
-    <main className="w-full h-full">
-      <Container>
-        <div className="justify-between flex mb-8">
-          <h1 className="flex items-center gap-4 text-3xl font-semibold">
-            Invoice {invoiceId}
-            <Badge
-              className={cn(
-                "rounded-full capitalize",
-                result.status === "open" && "bg-blue-500",
-                result.status === "paid" && "bg-green-600",
-                result.status === "void" && "bg-zinc-700",
-                result.status === "unpaid" && "bg-red-600"
-              )}
-            >
-              {result.status}
-            </Badge>
-          </h1>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button className="flex items-center gap-2" variant={"outline"}>
-                Change status
-                <ChevronDown className="w-4 h-auto" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              {AVAILABLE_STATUSES.map((status) => {
-                return (
-                  <DropdownMenuItem key={status.id}>
-                    <form action={updateStatus}>
-                      <input type="hidden" value={invoiceId} name="id" />
-                      <input type="hidden" value={status.id} name="status" />
-                      <button>{status.label}</button>
-                    </form>
-                  </DropdownMenuItem>
-                );
-              })}
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-
-        <p className="text-3xl mb-3">₹{(result.value / 100).toFixed(2)}</p>
-        <p className="text-lg mb-8">{result.description}</p>
-        <h2 className="font-bold text-lg mb-4">Billing Details</h2>
-
-        <ul className="grid gap-2">
-          <li className="flex gap-4">
-            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-              Invoice Id
-            </strong>
-            <span> {invoiceId} </span>
-          </li>
-          <li className="flex gap-4">
-            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-              Invoice Date
-            </strong>
-            <span>{new Date(result.createTs).toLocaleDateString()}</span>
-          </li>
-          <li className="flex gap-4">
-            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-              Billing Name
-            </strong>
-            <span></span>
-          </li>
-          <li className="flex gap-4">
-            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-              Billing Email
-            </strong>
-            <span></span>
-          </li>
-        </ul>
-      </Container>
-    </main>
-  );
+  return <Invoice invoice={result} />;
 }
